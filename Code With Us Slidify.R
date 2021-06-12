097aa655d147c09b52f7b2f83422efef79425bc2
maheshrao191100 <maheshrao191100@gmail.com>
2021-06-12 18:27:28 +0800

    completed

--- Code With Us Slidify.R
+++ Code With Us Slidify.R
@@ -3,11 +3,13 @@ library(devtools)
 
 install_github('ramnathv/slidify')
 install_github('ramnathv/slidifyLibraries')
+install_github('ramnathv/rCharts')
 
 setwd("C:/Users/Acer/Desktop/Code With Us")
+
 
 library(slidify)
 author("Code With Us Slidify")
 
-slidify('index.Rmd')
+slidify("index.Rmd")
 browseURL("index.html")
